15a4c5e53ead024531d912a1d13f7b68e19e4657
Evan-Jacobs-77 <evanalexjacob@gmail.com>
2021-09-28 21:19:36 -0500

    changed self.path to appropiate path in dpa/__init__.py

--- pygdpr/pygdpr/models/dpa/__init__.py
+++ pygdpr/pygdpr/models/dpa/__init__.py
@@ -103,7 +103,7 @@ class DPA(object):
         self.email = dpa['email']
         self.website = dpa['website']
         self.member = dpa['member']
-        self.path = '/Users/chen/PycharmProjects/gdpr-sota/documents' + path
+        self.path = '/Users/evanjacobs/Desktop/research_project/gdpr-sota/pygdpr' + path
         self.translate_client = None
 
     def set_path(self, path):
